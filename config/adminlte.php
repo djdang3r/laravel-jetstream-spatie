1f99d56aa29545627e780bf3cb1c3b288fe8442b
Script Develop CO <script.develop.co@gmail.com>
2024-12-22 17:58:34 -0500

    Dark mode and Footer fixed Adminlte

--- config/adminlte.php
+++ config/adminlte.php
@@ -156,7 +156,7 @@ return [
     'layout_boxed' => null,
     'layout_fixed_sidebar' => null,
     'layout_fixed_navbar' => null,
-    'layout_fixed_footer' => null,
+    'layout_fixed_footer' => true,
     'layout_dark_mode' => true,
 
     /*
@@ -198,7 +198,8 @@ return [
     'classes_content' => '',
     'classes_sidebar' => 'sidebar-dark-primary elevation-4',
     'classes_sidebar_nav' => '',
-    'classes_topnav' => 'navbar-white navbar-light',
+    // 'classes_topnav' => 'navbar-white navbar-light',
+    'classes_topnav' => 'navbar-dark',
     'classes_topnav_nav' => 'navbar-expand',
     'classes_topnav_container' => 'container',
 
